4f621c10d60ccabf78bd288d43a6ecda80684908
David3Emmanuel <david3emmanuel@gmail.com>
2025-09-08 15:29:15 +0100

    fix: add console logs for PORT, BACKEND_URL, and CLIENT_URL

--- ng-ecommerce/backend/constants.js
+++ ng-ecommerce/backend/constants.js
@@ -7,3 +7,7 @@ export const BACKEND_URL =
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
 export const CLIENT_URL = process.env.CLIENT_URL;
+
+console.log("PORT:", PORT);
+console.log("BACKEND_URL:", BACKEND_URL);
+console.log("CLIENT_URL:", CLIENT_URL);
